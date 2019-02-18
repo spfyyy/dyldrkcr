3a0e69599df1b8355f56bd8a6f6135541ad6a89a
Dylan Johnson <dylan@dylantjohnson.com>
2019-02-17 21:58:52 -0800

    Completed model for MediaInfo.
    This will contain the HLS stream URLs for videos.

---
+++ Shared/Models/MediaInfo.cs
@@ -0,0 +1,34 @@
+﻿using Newtonsoft.Json;
+
+namespace Shared.Models
+{
+    public class MediaInfo
+    {
+        [JsonProperty("data")]
+        public MediaInfoData Data { get; set; }
+
+        [JsonProperty("debug")]
+        public MediaInfoDebug Debug { get; set; }
+
+        [JsonProperty("error")]
+        public bool Error { get; set; }
+
+        [JsonProperty("code")]
+        public string Code { get; set; }
+
+        [JsonProperty("message")]
+        public string Message { get; set; }
+
+        public class MediaInfoData
+        {
+            [JsonProperty("stream_data")]
+            public StreamData StreamData { get; set; }
+        }
+
+        public class MediaInfoDebug
+        {
+            [JsonProperty("to_data")]
+            public double ToData { get; set; }
+        }
+    }
+}
---
+++ Shared/Models/StreamData.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+
+namespace Shared.Models
+{
+    public class StreamData
+    {
+        [JsonProperty("hardsub_lang")]
+        public string SubtitleLanguage { get; set; }
+
+        [JsonProperty("audio_lang")]
+        public string AudioLanguage { get; set; }
+        [JsonProperty("format")]
+        public string Format { get; set; }
+
+        [JsonProperty("streams")]
+        public List<Stream> Streams { get; set; }
+
+        public class Stream
+        {
+            [JsonProperty("quality")]
+            public string Quality { get; set; }
+
+            [JsonProperty("expires")]
+            public DateTime Expires { get; set; }
+
+            [JsonProperty("url")]
+            public string Url { get; set; }
+        }
+    }
+}
