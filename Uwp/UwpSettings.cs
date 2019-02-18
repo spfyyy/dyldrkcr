caad2ec4f2a7e50f46ab416c3f223cfa791bc45a
Dylan Johnson <dylan@dylantjohnson.com>
2019-02-17 23:22:13 -0800

    Implemented application settings.

---
+++ Uwp/UwpSettings.cs
@@ -0,0 +1,24 @@
+﻿using Shared;
+using Windows.Storage;
+
+namespace Uwp
+{
+    /// <summary>
+    /// UWP implementation of ISettings.
+    /// </summary>
+    public class UwpSettings : ISettings
+    {
+        private readonly ApplicationDataContainer _settings = ApplicationData.Current.LocalSettings;
+
+        public T Get<T>(SettingsKey key) where T : class
+        {
+            var result = _settings.Values[key.ToString()] as T;
+            return result;
+        }
+
+        public void Save(SettingsKey key, object value)
+        {
+            _settings.Values[key.ToString()] = value;
+        }
+    }
+}
