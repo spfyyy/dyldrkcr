52ed7876c0f8eb0e32960d795f4eb9262b8158be
spfyyy <spfyyy@gmail.com>
2019-02-19 11:58:57 -0800

    Clicking on a queue item shows a description.

--- Shared/ViewModels/QueueItemViewModel.cs
+++ Shared/ViewModels/QueueItemViewModel.cs
@@ -8,11 +8,24 @@ namespace Shared.ViewModels
 
         public int WatchPercentage { get; set; }
 
+        public string SeriesName { get; set; }
+
+        public string EpisodeTitle { get; set; }
+
+        public string Description { get; set; }
+
         public QueueItemViewModel(Queue.QueueItem item)
         {
             ThumbnailUrl = item.MostLikely.ScreenshotImage.FullUrl;
             var duration = (double)item.MostLikely.Duration;
             WatchPercentage = (int)(item.MostLikely.Playhead / duration * 100);
+            SeriesName = item.MostLikely.SeriesName;
+            EpisodeTitle = $"Episode {item.MostLikely.EpisodeNumber} - {item.MostLikely.Name}";
+            Description = item.MostLikely.Description;
+            if (Description.Length == 0)
+            {
+                Description = "No description.";
+            }
         }
     }
 }
